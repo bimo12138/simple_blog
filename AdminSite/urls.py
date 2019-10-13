0ab213305956a052ad17ccc314745ec1b4c9e3e7
bimo <1371639183@qq.com>
2019-10-13 22:54:10 +0800

    Initial BaseSite

---
+++ AdminSite/urls.py
@@ -0,0 +1,6 @@
+"""
+@author:      13716
+@date-time:   2019/10/13-22:51
+@ide:         PyCharm
+@name:        urls.py
+"""
--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -17,5 +17,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', include("AdminSite.urls")),
+    path("", include("IndexSite.urls"))
 ]
